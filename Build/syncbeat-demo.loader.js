1edffb54c88aa5e7626fc14d811e5ee710a8e7e0
Arshad <arshad1999@live.com>
2026-06-03 18:48:06 +0800

    Updated: v0.0.9.0 (Added World 5 and 6)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58704090,
+    wasmFileSize: 58706496,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
