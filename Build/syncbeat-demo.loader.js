5ddf576eb7ee393c0920286c32844a168830b808
Arshad <arshad1999@live.com>
2026-06-04 14:55:45 +0800

    Updated: v0.0.10.7 (Still trying to debug an existing bug)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58711303,
+    wasmFileSize: 58711402,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
