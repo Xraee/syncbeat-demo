c1d96c23d2520f92b7b1e897f1aeaed0917c29be
Arshad <arshad1999@live.com>
2026-06-05 11:43:03 +0800

    Updated: v0.0.10.13 (Final fixes to Mic UI)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58710769,
+    wasmFileSize: 58710991,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
