a8982231d029fe3fd34d1ae7d555df48b7b4ba2a
Arshad <arshad1999@live.com>
2026-06-15 15:58:50 +0800

    Updated: v0.0.13.1 (Fixed Reset Progress and Game UI)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58772763,
+    wasmFileSize: 58773150,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
