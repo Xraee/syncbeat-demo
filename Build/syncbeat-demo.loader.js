0f77c9abbb2431e18aa8f13d55f91a64913c3c2d
Arshad <arshad1999@live.com>
2026-06-17 15:48:37 +0800

    Updated: v0.0.14.1 (Fixed failing a 3rd level trigger complete world screen)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58784567,
+    wasmFileSize: 58784598,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
