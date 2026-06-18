b69e853abc5cf1b08febe117f1b37e940db10f93
Arshad <arshad1999@live.com>
2026-06-18 14:03:43 +0800

    Updated: v0.0.14.3 (Added a flashbar above HUD)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58784598,
+    wasmFileSize: 58786297,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
