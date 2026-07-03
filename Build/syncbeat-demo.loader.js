8b6a4dc5c4a73e144d15292c61a850f0168cf1d7
Arshad <arshad1999@live.com>
2026-07-03 17:35:48 +0800

    Updated: v0.0.18.1 (Add World 6, Shortcuts for Reset/Unlock levels)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58788823,
+    wasmFileSize: 58789286,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
