99702a99bdfaf354201549c80ecc15178256188e
Arshad <arshad1999@live.com>
2026-07-07 17:39:28 +0800

    Updated: v0.0.19 (Main Menu and World Map design changes)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58789286,
+    wasmFileSize: 58799509,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
