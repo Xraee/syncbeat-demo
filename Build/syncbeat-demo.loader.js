47f3eff7689b3ba9dc3eed4a0f2f133cece1e84d
Arshad <arshad1999@live.com>
2026-07-13 11:03:34 +0800

    Updated: v0.0.20.4 (Next/Previous buttons in World Map)

--- Build/syncbeat-demo.loader.js
+++ Build/syncbeat-demo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58799392,
+    wasmFileSize: 58800476,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
